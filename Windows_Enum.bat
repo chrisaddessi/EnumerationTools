f5a72aa4ec6eb19da34a4d4c0bf1e580207b9198
chrisaddessi <73910801+chrisaddessi@users.noreply.github.com>
2020-12-10 14:19:39 -0500

    Add files via upload

---
+++ Windows_Enum.bat
@@ -0,0 +1,9 @@
+hostname >> hostname.txt
+Ipconfig /all >> hostname.txt
+net localgroup Users >> hostname.txt
+netstat -an >> hostname.txt
+ver >> hostname.txt
+net localgroup Administrators >> hostname.txt
+net start >> hostname.txt
+netstat -abn >> hostname.txt
+more C:\Windows\System32\drivers\etc\hosts >> hostname.txt
